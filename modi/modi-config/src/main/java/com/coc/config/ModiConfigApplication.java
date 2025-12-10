214a5522950302c04419c7b5b5d026aabc32dc2d
metroz1 <wlstjr132@gmail.com>
2025-12-10 17:25:44 +0900

    feat: geteway, discovery, config 서버 생성 및 미사용 Cart 도메인 삭제
    - modi-gateway에 OpenApiDocConfig 추가하여 문서화 설정 적용
    - rental-service에서 불필요한 Cart 도메인 클래스 삭제
    - settings.gradle 변경 사항 반영

---
+++ modi/modi-config/src/main/java/com/coc/config/ModiConfigApplication.java
@@ -0,0 +1,14 @@
+package com.coc.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModiConfigApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ModiConfigApplication.class, args);
+	}
+	
+}
---
+++ modi/modi-discovery/src/main/java/com/coc/discovery/ModiDiscoveryApplication.java
@@ -0,0 +1,14 @@
+package com.coc.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModiDiscoveryApplication {
+	
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ModiDiscoveryApplication.class, args);
+	}
+	
+}
