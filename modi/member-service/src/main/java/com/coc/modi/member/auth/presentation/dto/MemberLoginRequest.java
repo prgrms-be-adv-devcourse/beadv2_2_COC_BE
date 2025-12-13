c07e8cea7790b3597c1d0abd302ee9a8339365b8
carsumin <carsumin@gmail.com>
2025-12-14 01:27:02 +0900

    refactor: 회원가입시 패스워드 유효성 체크 추가

--- modi/member-service/src/main/java/com/coc/modi/member/auth/presentation/dto/MemberLoginRequest.java
+++ modi/member-service/src/main/java/com/coc/modi/member/auth/presentation/dto/MemberLoginRequest.java
@@ -4,6 +4,7 @@ import com.coc.modi.member.auth.application.dto.MemberLoginCommand;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record MemberLoginRequest(
 		@NotBlank
@@ -11,6 +12,10 @@ public record MemberLoginRequest(
 		String email,
 		
 		@NotBlank
+		@Pattern(
+				regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[!@#$%^&*()_+=-]).{8,20}$",
+				message = "비밀번호는 8~20자이며 영문, 숫자, 특수문자를 모두 포함해야 합니다."
+		)
 		String password
 ) {
 	public MemberLoginCommand toCommand() {
