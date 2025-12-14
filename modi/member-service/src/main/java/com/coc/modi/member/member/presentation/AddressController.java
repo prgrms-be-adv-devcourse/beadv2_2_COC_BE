1bbb1e84606bf80e27aeb5f3eb6ce54866d8e089
metroz1 <wlstjr132@gmail.com>
2025-12-15 02:13:36 +0900

    fix: AddressController Api 리턴 부분 빠진 ) 추가

--- modi/member-service/src/main/java/com/coc/modi/member/member/presentation/AddressController.java
+++ modi/member-service/src/main/java/com/coc/modi/member/member/presentation/AddressController.java
@@ -13,7 +13,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,7 +27,7 @@ public class AddressController {
     @GetMapping("/profile")
     public ResponseEntity<ApiResponse<List<AddressResponse>>> getProfileAddresses(@AuthenticationPrincipal CustomMember member) {
 		
-        return ResponseEntity.ok(ApiResponse.ok(addressService.getProfileAddresses(member.getMemberId());
+        return ResponseEntity.ok(ApiResponse.ok(addressService.getProfileAddresses(member.getMemberId())));
     }
 
 	// 주소 등록
