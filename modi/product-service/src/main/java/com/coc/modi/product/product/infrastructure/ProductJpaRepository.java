d46393c0ebffbf44879feafd67412ffbb0e15ff8
cje206 <projec206@gmail.com>
2025-12-15 11:47:18 +0900

    refactor: 공통 sellerId 조회 분리 및 상품 조회/동기화 로직 개선
    - getSellerId 로직을 application 공통 컴포넌트로 분리
    - 판매자 렌탈 상품 리스트 조회 API 추가
    - 상품 수정 시 Elasticsearch 반영 누락 문제 수정
    - 상품 단건 조회 시 delete 상태 상품 조회 제외
    - 상품 단건 조회 시 inactive 상태는 판매자 본인에게만 노출되도록 처리

--- modi/product-service/src/main/java/com/coc/modi/product/product/infrastructure/ProductJpaRepository.java
+++ modi/product-service/src/main/java/com/coc/modi/product/product/infrastructure/ProductJpaRepository.java
@@ -1,13 +1,21 @@
 package com.coc.modi.product.product.infrastructure;
 
 import com.coc.modi.product.product.domain.Product;
+import com.coc.modi.product.product.domain.ProductStatus;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductJpaRepository extends JpaRepository<Product, Long> {
+	
+	Page<Product> findBySellerIdAndStatusNot(Long sellerId, ProductStatus status, Pageable pageable);
+	
+	Optional<Product> findByIdAndStatusNot(Long id, ProductStatus status);
 	
 	List<Product> findByIdIn(Collection<Long> ids);
 }
