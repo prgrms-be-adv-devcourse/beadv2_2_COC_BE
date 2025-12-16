d86d7ed04fd8beec191e9450556864e852cbbb71
metroz1 <wlstjr132@gmail.com>
2025-12-17 03:31:39 +0900

    fix: 렌탈 조회에 createdAt 추가

--- modi/rental-service/src/main/java/com/coc/modi/rental/rental/application/dto/RentalResponse.java
+++ modi/rental-service/src/main/java/com/coc/modi/rental/rental/application/dto/RentalResponse.java
@@ -8,11 +8,12 @@ import java.util.List;
 public record RentalResponse(
 		Long rentalId,
 		LocalDateTime paidAt,
+		LocalDateTime createdAt,
 		List<RentalItemResponse> items
 ) {
 	
 	public static RentalResponse create(Rental rental, List<RentalItemResponse> rentalItemResponseList) {
 		
-		return new RentalResponse(rental.getId(), rental.getPaidAt(), rentalItemResponseList);
+		return new RentalResponse(rental.getId(), rental.getPaidAt(), rental.getCreatedAt(), rentalItemResponseList);
 	}
 }
