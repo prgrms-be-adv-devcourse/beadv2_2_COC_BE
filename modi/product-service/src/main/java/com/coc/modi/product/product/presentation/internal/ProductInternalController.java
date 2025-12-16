c79d563f5dcb07b083444f988e87f941072f8ba5
hyeonguk2 <jim9823@naver.com>
2025-12-17 00:05:07 +0900

    fix : change getProductsById from ProductInternalController

--- modi/product-service/src/main/java/com/coc/modi/product/product/presentation/internal/ProductInternalController.java
+++ modi/product-service/src/main/java/com/coc/modi/product/product/presentation/internal/ProductInternalController.java
@@ -6,12 +6,7 @@ import com.coc.modi.product.product.application.dto.ProductInternalSellerRespons
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,8 +23,8 @@ public class ProductInternalController {
 		return productService.getProductsByIds(productIds);
 	}
 	
-	@GetMapping
-	public ProductInternalSellerResponse getProductsById(@RequestParam("productId") Long productId) {
+	@GetMapping("/{productId}")
+	public ProductInternalSellerResponse getProductsById(@PathVariable("productId") Long productId) {
 		
 		return productService.getProductById(productId);
 	}
