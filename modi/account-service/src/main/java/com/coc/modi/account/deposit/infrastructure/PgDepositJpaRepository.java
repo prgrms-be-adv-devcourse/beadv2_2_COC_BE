7621c890b05f32854f3ba9f78111d67c497aff1f
metroz1 <wlstjr132@gmail.com>
2025-12-24 16:29:32 +0900

    feat: 결제 동시성 제어(락) 적용 및 Tilt 적용

--- modi/account-service/src/main/java/com/coc/modi/account/deposit/infrastructure/PgDepositJpaRepository.java
+++ modi/account-service/src/main/java/com/coc/modi/account/deposit/infrastructure/PgDepositJpaRepository.java
@@ -2,14 +2,23 @@ package com.coc.modi.account.deposit.infrastructure;
 
 import com.coc.modi.account.deposit.domain.PgDeposit;
 import com.coc.modi.account.deposit.domain.PgDepositStatus;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
+import jakarta.persistence.LockModeType;
+
 public interface PgDepositJpaRepository extends JpaRepository<PgDeposit, Long> {
 
     Optional<PgDeposit> findByPgTid(String pgTid);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select p from PgDeposit p where p.pgTid = :pgTid")
+    Optional<PgDeposit> findByPgTidForUpdate(@Param("pgTid") String pgTid);
 
     List<PgDeposit> findByMemberId(Long memberId);
 
