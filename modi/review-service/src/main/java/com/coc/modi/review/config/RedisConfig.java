e8f1f60b5de2fe50fe4dd7ea5c152f02f0ff6a8c
전형욱 <jim9823@naver.com>
2026-01-16 17:37:23 +0900

    fix: redisconfig for review

--- modi/review-service/src/main/java/com/coc/modi/review/config/RedisConfig.java
+++ modi/review-service/src/main/java/com/coc/modi/review/config/RedisConfig.java
@@ -4,24 +4,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import com.coc.modi.review.cache.ReturnedRentalItem;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class RedisConfig {
 
 	@Bean
+	public ObjectMapper redisObjectMapper() {
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+		return objectMapper;
+	}
+
+	@Bean
 	public RedisTemplate<String, ReturnedRentalItem> returnedRentalRedisTemplate(
-			RedisConnectionFactory connectionFactory
+			RedisConnectionFactory connectionFactory,
+			ObjectMapper redisObjectMapper
 	) {
 		RedisTemplate<String, ReturnedRentalItem> template = new RedisTemplate<>();
 		template.setConnectionFactory(connectionFactory);
 
 		StringRedisSerializer stringSerializer = new StringRedisSerializer();
-		Jackson2JsonRedisSerializer<ReturnedRentalItem> jsonSerializer =
-				new Jackson2JsonRedisSerializer<>(ReturnedRentalItem.class);
+		GenericJackson2JsonRedisSerializer jsonSerializer =
+				new GenericJackson2JsonRedisSerializer(redisObjectMapper);
 
 		template.setKeySerializer(stringSerializer);
 		template.setValueSerializer(jsonSerializer);
