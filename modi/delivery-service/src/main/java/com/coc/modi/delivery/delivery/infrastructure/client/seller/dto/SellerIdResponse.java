c2d29458b8ff6242a0c837b68bb2897a08322d90
cje206 <projec206@gmail.com>
2026-01-20 13:48:45 +0900

    refactor: feign client 예외 처리 추가

--- modi/delivery-service/src/main/java/com/coc/modi/delivery/delivery/infrastructure/client/seller/dto/SellerIdResponse.java
+++ modi/delivery-service/src/main/java/com/coc/modi/delivery/delivery/infrastructure/client/seller/dto/SellerIdResponse.java
@@ -1,7 +1,16 @@
 package com.coc.modi.delivery.delivery.infrastructure.client.seller.dto;
 
+import com.coc.modi.common.ErrorCode;
+import com.coc.modi.delivery.delivery.exception.DeliveryException;
+
 public record SellerIdResponse(
 		Long sellerId,
 		Long memberId
 ) {
+	public SellerIdResponse {
+		
+		if (sellerId == null || memberId == null) {
+			throw new DeliveryException(ErrorCode.DELIVERY_INVALID_INPUT, "판매자/회원 정보는 필수입니다.");
+		}
+	}
 }
