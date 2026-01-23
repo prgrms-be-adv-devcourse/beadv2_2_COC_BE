b05519ce7f116e9f6d78dae7c000573caba67785
beekeeper24 <ahwnsk94@gmail.com>
2026-01-23 12:05:58 +0900

    import 추가

--- modi/seller-service/src/main/java/com/coc/modi/seller/settlement/domain/SellerSettlementLine.java
+++ modi/seller-service/src/main/java/com/coc/modi/seller/settlement/domain/SellerSettlementLine.java
@@ -4,6 +4,8 @@ import com.coc.modi.common.BaseEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
